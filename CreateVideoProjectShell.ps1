6116e67fd5e76e950191bb13aa456f11949988d4
Chris Allermann <callermann@gmail.com>
2025-08-22 10:03:30 -0400

    Initial commit

---
+++ CreateVideoProjectShell.ps1
@@ -0,0 +1,32 @@
+﻿$subDirs = "After Effects", "Clips", "Exports", "Images", "Music", "Sound Effects", "Thumbnails"
+
+
+if($args.Count -eq 1) {
+    Write-Host "No Paths Specified"
+}
+else {
+
+    for ($i=1; $i -lt $args.Count; $i++) {
+        $basePath = $args[$i]
+        if(Test-Path -Path $basePath -PathType Container) {
+            Write-Host -ForegroundColor Green "Creating Subdirectories in '$basePath'"
+            foreach($subDir in $subDirs) {
+                $newPath = Join-Path $basePath -ChildPath $subDir
+                if(Test-Path -Path $newPath) {
+                    Write-Host -ForegroundColor Red "  - Skipping existing subdirectory $subDir"
+                }
+                else {
+                    Write-Host -ForegroundColor Green "  - Creating Subdirectory: $subDir"
+                    New-Item -Path $newPath -ItemType Directory | Out-Null
+                }
+
+            }
+
+        }
+        else {
+            Write-Host -ForegroundColor Red "Basepath: '$basePath' is not a directory, skipping"
+        }
+    }
+}
+
+Read-Host "Press Enter to continue..."
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # video-project-shell-creation
 Script To Create Project Structure for Video Editing Project
+
+## Install Instructions
+1) Place script in desired location.
+2) Open ``shell:sendto `` location.
+3) Create new shortcut with the following target<br>``C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File "<script path>" %``<br>
+Replace ``<script path>`` with location of where you saved the script.
+
+## To Run
+Right click on folder where you want to create the video project shell and select the Create Video Project Shell Shortcut
